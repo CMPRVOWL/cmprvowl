ef1df81e20e7d789a812ee8e51373bf00c7f7067
CMPRVOWL <61151424+CMPRVOWL@users.noreply.github.com>
2020-04-02 14:10:16 +0100

    Patched Login.php

--- login.php
+++ login.php
@@ -48,9 +48,11 @@ session_start();
 // If form submitted, insert values into the database.
 if (isset($_POST['username'])){
         // removes backslashes
- $username = ($_REQUEST['username']);
+ $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-$password = ($_REQUEST['password']);
+ $username = mysqli_real_escape_string($con,$username);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($con,$password);
  $pwsalt = "aksdjbflauiybclaudhygacou";
  //Checking is user existing in the database or not
         $query = "SELECT * FROM `users` WHERE username='$username'
