22bfb2c98acc67ead6573f64005413d8ed44be78
CMPRVOWL <61151424+CMPRVOWL@users.noreply.github.com>
2020-04-02 14:40:56 +0100

    Patched Data.php

--- data.php
+++ data.php
@@ -17,28 +17,16 @@
 	
 
 	if (isset($_POST['save'])) {
-		// Strip back slashes
-		$Cwk_Title = stripslashes($_POST['Cwk_Title']);
-		$Mod_Name = stripslashes($_POST['Mod_Name']);
-		$Mod_Code = stripslashes($_POST['Mod_Code']);
-		$Max_Mark = stripslashes($_POST['Max_Mark']);
-		$Lecturer = stripslashes($_POST['Lecturer']);
-		$HandO_date = stripslashes($_POST['HandO_date']);
-		$HandI_Date = stripslashes($_POST['HandI_Date']);
-		$FeedB_date = stripslashes($_POST['FeedB_date']);
-		$Completed = stripslashes($_POST['Completed']);
-		$Result = stripslashes($_POST['Result']);
-		// Escape Special characters
-		$Cwk_Title = mysqli_real_escape_string($db,$Cwk_Title);
-		$Mod_Name = mysqli_real_escape_string($db,$Mod_Name);
-		$Mod_Code = mysqli_real_escape_string($db,$Mod_Code);
-		$Max_Mark = mysqli_real_escape_string($db,$Max_Mark);
-		$Lecturer = mysqli_real_escape_string($db,$Lecturer);
-		$HandO_date = mysqli_real_escape_string($db,$HandO_date);
-		$HandI_Date = mysqli_real_escape_string($db,$HandI_Date);
-		$FeedB_date = mysqli_real_escape_string($db,$FeedB_date);
-		$Completed = mysqli_real_escape_string($db,$Completed);
-		$Result = mysqli_real_escape_string($db,$Result);
+		$Cwk_Title = $_POST['Cwk_Title'];
+		$Mod_Name = $_POST['Mod_Name'];
+		$Mod_Code = $_POST['Mod_Code'];
+		$Max_Mark = $_POST['Max_Mark'];
+		$Lecturer = $_POST['Lecturer'];
+		$HandO_date = $_POST['HandO_date'];
+		$HandI_Date = $_POST['HandI_Date'];
+		$FeedB_date = $_POST['FeedB_date'];
+		$Completed = $_POST['Completed'];
+		$Result = $_POST['Result'];
 		
 		mysqli_query($db, "INSERT INTO assignments (Cwk_Title, Mod_Name, Mod_Code, Max_Mark, Lecturer, HandO_date ,HandI_Date, FeedB_date, Completed, Result) VALUES ('$Cwk_Title', '$Mod_Name', '$Mod_Code', '$Max_Mark', '$Lecturer', '$HandO_date', '$HandI_Date', '$FeedB_date', '$Completed', '$Result')"); 
 		$_SESSION['message'] = "Assignment Details Saved!"; 
@@ -46,30 +34,17 @@
 	}
 		
 	if (isset($_POST['update'])) {
-		// Strip back slashes
-		$id = stripslashes($_POST['id']);
-		$Cwk_Title = stripslashes($_POST['Cwk_Title']);
-		$Mod_Name = stripslashes($_POST['Mod_Name']);
-		$Mod_Code = stripslashes($_POST['Mod_Code']);
-		$Max_Mark = stripslashes($_POST['Max_Mark']);
-		$Lecturer = stripslashes($_POST['Lecturer']);
-		$HandO_date = stripslashes($_POST['HandO_date']);
-		$HandI_Date = stripslashes($_POST['HandI_Date']);
-		$FeedB_date = stripslashes($_POST['FeedB_date']);
-		$Completed = stripslashes($_POST['Completed']);
-		$Result = stripslashes($_POST['Result']);
-		// Escape Special characters
-		$id = mysqli_real_escape_string($db,$id);
-		$Cwk_Title = mysqli_real_escape_string($db,$Cwk_Title);
-		$Mod_Name = mysqli_real_escape_string($db,$Mod_Name);
-		$Mod_Code = mysqli_real_escape_string($db,$Mod_Code);
-		$Max_Mark = mysqli_real_escape_string($db,$Max_Mark);
-		$Lecturer = mysqli_real_escape_string($db,$Lecturer);
-		$HandO_date = mysqli_real_escape_string($db,$HandO_date);
-		$HandI_Date = mysqli_real_escape_string($db,$HandI_Date);
-		$FeedB_date = mysqli_real_escape_string($db,$FeedB_date);
-		$Completed = mysqli_real_escape_string($db,$Completed);
-		$Result = mysqli_real_escape_string($db,$Result);
+		$id = $_POST['id'];
+		$Cwk_Title = $_POST['Cwk_Title'];
+		$Mod_Name = $_POST['Mod_Name'];
+		$Mod_Code = $_POST['Mod_Code'];
+		$Max_Mark = $_POST['Max_Mark'];
+		$Lecturer = $_POST['Lecturer'];
+		$HandO_date = $_POST['HandO_date'];
+		$HandI_Date = $_POST['HandI_Date'];
+		$FeedB_date = $_POST['FeedB_date'];
+		$Completed = $_POST['Completed'];
+		$Result = $_POST['Result'];
 
 		mysqli_query($db, "UPDATE assignments SET Cwk_Title='$Cwk_Title', Mod_Name='$Mod_Name', Mod_Code='$Mod_Code', Max_Mark='$Max_Mark', Lecturer='$Lecturer', HandO_date='$HandO_date', HandI_Date='$HandI_Date', FeedB_date='$FeedB_date', Completed='$Completed', Result='$Result' WHERE id=$id");
 		$_SESSION['message'] = "Assignment Details updated!"; 
@@ -77,12 +52,7 @@
 	}
 	if (isset($_GET['del'])) {
 	$id = $_GET['del'];
-				// Strip back slashes
-		$id = stripslashes($_GET['del']);
-		$id = mysqli_real_escape_string($db,$id);
-	
 	mysqli_query($db, "DELETE FROM assignments WHERE id=$id");
-
 	$_SESSION['message'] = "Assignment Deleted!"; 
 	header('location: ViewAss.php');
 }
